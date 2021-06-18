0dc7d24fddc535f3520a6ef847949443ad7be75f
tusher16 <tusher16@gmail.com>
2021-06-18 17:54:15 +0200

    insstaled sqlclient and added the database setting

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tusherco_django_portfolio',
+        'USER': 'tusherco_portfolio',
+        'PASSWORD': 'tusher1116$',
+        'DB': 'lychee.exonhost.com',
+        'HOST' : 'lychee.exonhost.com',
+        'PORT': '3306',
     }
 }
 
