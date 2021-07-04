73e4018cec63109c4c880d3d3a9514809dd17af2
tusher16 <tusher16@gmail.com>
2021-07-04 12:25:38 +0200

    added static homepage and added all the html csss and js files.

--- home_page/views.py
+++ home_page/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+	return render(request, 'home_page/index.html')
+	# remove this line #return HttpResponse("hello. this is test request")
+
+
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,9 +124,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    #'/var/www/static/',
+]
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
