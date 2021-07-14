c2bd4fa183fa4846a346ff63d7859b36a6fbbbc0
tusher16 <tusher16@gmail.com>
2021-07-14 15:24:53 +0200

    moved mysql to sqlite

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,13 +80,15 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tusherco_django_portfolio',
-        'USER': 'tusherco_portfolio',
-        'PASSWORD': 'tusher1116$',
-        'DB': 'lychee.exonhost.com',
-        'HOST' : 'lychee.exonhost.com',
-        'PORT': '3306',
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'tusherco_django_portfolio',
+#        'USER': 'tusherco_portfolio',
+#        'PASSWORD': 'tusher1116$',
+#        'DB': 'lychee.exonhost.com',
+#        'HOST' : 'lychee.exonhost.com',
+#        'PORT': '3306',
+	'ENGINE': 'django.db.backends.sqlite3',
+	'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
