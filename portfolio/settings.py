6c30f0b90652beca73574144a85ae968852d45cb
tusher16 <tusher16@gmail.com>
2021-07-14 14:43:56 +0200

    removed var/www/static from settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,8 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
+    BASE_DIR / "static"
 ]
 
 MEDIA_URL = '/images/'
